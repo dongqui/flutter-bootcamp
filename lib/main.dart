c9731430afecb1c2061e19c8b647ae12e23b0eea
Dongjin <dongjin@plask.ai>
2023-09-27 23:27:16 +0900

    icons

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'),
+            image: AssetImage('images/다운로드.jpg'),
           ),
         ),
       ),
